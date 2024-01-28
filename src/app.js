ac0522551d8d727fc05756eea50ed289cfe784bc
FranciscoBisso <fjbiss@gmail.com>
2024-01-28 17:57:53 -0300

    initialize node project. Install express, nodemon & dotenv

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 3001;
+
+app.use(express.static("public"));
+
+app.listen(port, () => {
+	console.log(`App listening port: ${port}!`);
+});
